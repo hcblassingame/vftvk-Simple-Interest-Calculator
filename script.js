4e2d32c1f04e7ec90802d34d02f3087af44f5cc2
hcblassingame <56264596+hcblassingame@users.noreply.github.com>
2022-05-10 19:05:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,29 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
- 
+    var amt = parseInt(principal) + parseInt(interest);
+    var result = document.getElementById("result")
+
+    result.innerHTML = "If you deposit <mark> "+principal+" </mark> <br>";
+    result.innerHTML += "at an interest rate of <mark>" + rate + "% </mark> <br>";
+    result.innerHTML += "You will receive an amount of <mark>" + amt + "</mark> <br>";
+    result.innerHTML += "In the year <mark>" + year + "</mark>. <br>"
+
 }
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
 }  
-        
+
+function validate() {
+
+        var princip_val = document.getElementById("principal");
+
+        if (princip_val.value == "" || princip_val.value < 0) {
+            alert("The principal must be greater than 0");
+            princip_val.focus();
+            return false;
+        }
+        return true;
+}
